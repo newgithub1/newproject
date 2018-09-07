d47e985c69406d61b4a5d6ed1320e650627d5334
gitsampl <dharapriya215@gmail.com>
2018-09-07 14:57:25 +0530

    function added

--- empty dp/src/main/webapp/js/myRecords.js
+++ empty dp/src/main/webapp/js/myRecords.js
@@ -11,5 +11,15 @@ function myDatas(res){
 	}
 	document.getElemntById("dp").innerHTML=temp;
 	
-	document.getElementById("demo").innerHTML=a;
+}
+function myDatas(res){
+	alert(res);
+	temp="";
+	if(res!=null && res.length>0){
+		for(var i=0;i>res.length;i++){
+			temp='<tr><td>'+ res[i].sid+'</td><td>'+res[i].college+'</td><td>'+res[i].nm_fk+'</td><td>'+res[i].name+'</td></tr>';
+		}
+	}
+	document.getElemntById("dp").innerHTML=temp;
+	
 }
